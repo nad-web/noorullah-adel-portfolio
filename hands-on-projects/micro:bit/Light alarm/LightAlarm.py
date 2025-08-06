cfa904ab2fa0046d828d2421311179f9c183ca9a
Noorullah Adel <118401596+nad-web@users.noreply.github.com>
2025-08-06 13:34:17 +0200

    Update LightAlarm.py

--- hands-on-projects/micro:bit/Light alarm/LightAlarm.py
+++ hands-on-projects/micro:bit/Light alarm/LightAlarm.py
@@ -1,1 +1,32 @@
+# Sensor / transmitter:
 
+from microbit import *
+import radio
+radio.config(group=5)
+radio.on()
+
+while True:
+    if button_a.was_pressed():
+        display.scroll(display.read_light_level())
+    if display.read_light_level() > 50:
+        radio.send('lights on')
+    else:
+        radio.send('lights off')
+    sleep(10000)
+  
+# Alarm / receiver:
+
+from microbit import *
+import music
+import radio
+radio.config(group=5)
+radio.on()
+
+while True:
+    message = radio.receive()
+    if message:
+        if message == 'lights off':
+            display.clear()
+        elif message == 'lights on':
+            display.show(Image.ANGRY)
+            music.play(music.BADDY)
